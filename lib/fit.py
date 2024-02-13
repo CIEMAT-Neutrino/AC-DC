f399c7415d70de1971494f1e6da5743a5bc495ae
mantheys <sergio.manthey@ciemat.es>
2024-02-13 18:16:04 +0100

    FIT UPDATE TO AVOID DC DETECTED AS BURSTS

--- lib/fit.py
+++ lib/fit.py
@@ -4,7 +4,11 @@ import stat
 import json
 import warnings
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+
+from itertools import product
+from rich import print as rprint
 from scipy.optimize import curve_fit
 # suppress warnings 
 warnings.filterwarnings('ignore')
@@ -12,65 +16,83 @@ warnings.filterwarnings('ignore')
 from src.utils import get_project_root
 root = get_project_root()
 
+
 # TF1* fG1 = new TF1("fG1","[1]*[0]*log(10)*(10**x)*exp(-[0]*(10**x))");
 def dark_current_SiPM(x, A, B):
     return A * np.log(10) * (10**x) * np.exp(-B * (10**x))
+
 # TF1* fG2 = new TF1("fG2","[1]*[2]*(([2]-1)/[2])*(10**x)*log(10)*(1+([0]*(10**x))/[2])**(-[2])");
 def bursts_SiPM(x, C, D, E):
-    return D * ((E-1)/E) * (10**x) * np.log(10) * (1 + (C * (10**x)) / E)**(-E)    
+    return C * ((E-1)/E) * (10**x) * np.log(10) * (1 + (D * (10**x)) / E)**(-E)    
+
 # Combine the two functions
 def SiPM_response(x, A, B, C, D, E):
     return dark_current_SiPM(x, A, B) + bursts_SiPM(x, C, D, E)
 
-def fit_SiPM_response(df,filter_data:bool=False,save:bool=False,debug:bool=False):
-    n = df['Number'].iloc[0]
-    label = df['Set'].iloc[0]
-    ov = df['OV'].iloc[0]
-    ch = df['Channel'].iloc[0]
-    # Append new df to existing df and add n,label,ov and ch as new columns with the same length as the new df
-    df['Frequency'] = 1/df['DeltaT']
-    df['LogDeltaT'] = np.log10(df['DeltaT'])
 
-    # Make a histogram of the frequency using np.histogram
-    amp_lim = df["Amplitude"].max()
-    if filter_data:
-        with open('{root}/data/analysis/SPE.json') as json_file:
-            SPE_data = json.load(json_file)
-        amp_lim = SPE_data[str(n)][label]["OV"][str(ov)]
+def fit_SiPM_response(df:pd.DataFrame,filter_data:bool=False,save:bool=False,debug:bool=False) -> pd.DataFrame:
+    numbers = df['Number'].unique()
+    labels = df['Set'].unique()
+    ovs = df['OV'].unique()
+    chs = df['Channel'].unique()
 
-    hist, bin_edges = np.histogram(df[df["Amplitude"] < amp_lim]["LogDeltaT"], bins=200)
-    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+    df['Frequency'] = 0
+    df['LogDeltaT'] = 0
+    for n, label, ov, ch in product(numbers, labels, ovs, chs): # type: ignore
+        data = df[(df['Number'] == n) & (df['Set'] == label) & (df['OV'] == ov) & (df['Channel'] == ch)]
+        # Append new df to existing df and add n,label,ov and ch as new columns with the same length as the new df
+        data['Frequency'] = 1/data['DeltaT']
+        data['LogDeltaT'] = np.log10(data['DeltaT'])
+        # Update df with Frequency and LogDeltaT
+        df.loc[(df['Number'] == n) & (df['Set'] == label) & (df['OV'] == ov) & (df['Channel'] == ch), 'Frequency'] = data['Frequency']
+        df.loc[(df['Number'] == n) & (df['Set'] == label) & (df['OV'] == ov) & (df['Channel'] == ch), 'LogDeltaT'] = data['LogDeltaT']
+        # Make a histogram of the frequency using np.histogram
+        amp_lim = data["Amplitude"].max()
+        if filter_data:
+            with open('{root}/data/analysis/SPE.json') as json_file:
+                SPE_data = json.load(json_file)
+            amp_lim = SPE_data[str(n)][label]["OV"][str(ov)]
 
-    try:
-        popt, pcov = curve_fit(SiPM_response, bin_centers, hist, p0=[1e2,1,1e2,1e4,2], bounds=([0,0,0,0,0],[1e3,10,1e3,1e6,10]))
-    except RuntimeError:
-        print(f'Fit failed for {n} {label} OV{ov} Ch{ch}')
-        return
-    perr = np.sqrt(np.diag(pcov))
+        hist, bin_edges = np.histogram(data[data["Amplitude"] < amp_lim]["LogDeltaT"], bins=200)
+        bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
 
-    # Plot the histogram and the fit
-    fig, ax = plt.subplots()
-    ax.plot(bin_centers, hist, label='Data', drawstyle='steps-mid')
-    ax.plot(bin_centers, SiPM_response(bin_centers, *popt), label='Fit')
-    ax.fill_between(bin_centers, SiPM_response(bin_centers, *(popt + perr)), SiPM_response(bin_centers, *(popt - perr)), alpha=0.2)
-    ax.plot(bin_centers, dark_current_SiPM(bin_centers, *popt[:2]), label='DarkCurrent',ls='--')
-    ax.plot(bin_centers, bursts_SiPM(bin_centers, *popt[2:]), label='Bursts',ls='--')
-    ax.set_title(f'SiPM response fit for {n} {label} OV{ov} Ch{ch}')
-    ax.set_xlabel('log10(DeltaT)')
-    ax.set_ylabel('Counts')
-    ax.legend()
+        try:
+            popt, pcov = curve_fit(SiPM_response, bin_centers, hist, p0=[1e2,1,1e2,1e2,2], bounds=([0,1e-2,0,5e1,0],[1e6,5e1,1e6,1e4,10]))
+        except RuntimeError:
+            rprint(f'Fit failed for {n} {label} OV{ov} Ch{ch}')
+            return df
+        perr = np.sqrt(np.diag(pcov))
 
-    if save:
-        plt.savefig(f'{root}/data/images/{n}/{label}/DC_data_fit_{ov}_{ch}.png', dpi=300)
-        plt.close()
-        with open(f'{root}/data/{n}/{label}/DC_data_fit_{ov}_{ch}.csv', 'w') as file:
-            file.write(f'Number,Set,OV,Channel,A,dA,DarkCurrent,dDarkCurrent,Bursts,dBursts,D,dD,E,dE\n')
-            file.write(f'{n},{label},{ov},{ch},{popt[0]},{perr[0]},{popt[1]},{perr[1]},{popt[2]},{perr[2]},{popt[3]},{perr[3]},{popt[4]},{perr[4]}\n')
-        file.close()
-        os.chmod(f'{root}/data/{n}/{label}/DC_data_fit_{ov}_{ch}.csv', stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+        # Plot the histogram and the fit
+        fig, ax = plt.subplots()
+        ax.plot(bin_centers, hist, label='Data', drawstyle='steps-mid')
+        ax.plot(bin_centers, SiPM_response(bin_centers, *popt), label='Fit')
+        ax.plot(bin_centers, dark_current_SiPM(bin_centers, *popt[:2]), label='DarkCurrent',ls='--')
+        ax.plot(bin_centers, bursts_SiPM(bin_centers, *popt[2:]), label='Bursts',ls='--')
+        ax.set_title(f'SiPM response fit for {n} {label} OV{ov} Ch{ch}')
+        ax.set_xlabel('log10(DeltaT)')
+        ax.set_ylabel('Counts')
+        ax.legend()
+
+        if save:
+            image_path = f'{root}/data/images/{n}/{label}/'
+            os.system(f'mkdir -p {image_path}')
+            plt.savefig(f'{image_path}/{n}_{label}_DC_data_fit_{ov}_{ch}.png', dpi=300)
+            plt.close()
+            with open(f'{root}/data/{n}/{label}/{n}_{label}_DC_data_fit_{ov}_{ch}.csv', 'w') as file:
+                file.write(f'Number,Set,OV,Channel,A,dA,DarkCurrent,dDarkCurrent,B,dB,Bursts,dBursts,E,dE\n')
+                file.write(f'{n},{label},{ov},{ch},{popt[0]},{perr[0]},{popt[1]},{perr[1]},{popt[2]},{perr[2]},{popt[3]},{perr[3]},{popt[4]},{perr[4]}\n')
+            file.close()
+            os.chmod(f'{root}/data/{n}/{label}/{n}_{label}_DC_data_fit_{ov}_{ch}.csv', stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+        
+    
+    if debug: rprint(f'Updated df with Frequency and LogDeltaT')
+    return df
+
 
 def gauss(x,a,x0,sigma):
     return a*np.exp(-0.5*np.power((x-x0)/sigma,2))
+
 
 def gaussian_train(x, *params):
     y = np.zeros_like(x)
@@ -78,8 +100,9 @@ def gaussian_train(x, *params):
         height = params[i]
         center = params[i+1]
         width  = params[i+2]
-        y      +=  gauss(x, height, center, width)
+        y += gauss(x, height, center, width)
     return y
+
 
 def fit_gaussians(x, y, *p0):
     assert x.shape == y.shape, "Input arrays must have the same shape."
@@ -88,8 +111,8 @@ def fit_gaussians(x, y, *p0):
     chi_squared = np.sum((y[abs(fit_y)>0.1] - fit_y[abs(fit_y)>0.1]) ** 2 / fit_y[abs(fit_y)>0.1]) / (y.size - len(popt))
     return popt,fit_y, chi_squared
     
+    
 def Q_out(V_out,period):
-    #   V_out in Volts
-    #          osc resistance    x  t
-    q=V_out  /      50          *   period
+    # Volts / osc resistance * time
+    q=V_out/50*period
     return q
