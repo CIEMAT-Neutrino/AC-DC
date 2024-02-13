ececd5d98e08e643a75c2d6202be4862b32351bf
mantheys <sergio.manthey@ciemat.es>
2024-02-13 12:16:10 +0100

    INCLUDE ROOT DIRECTORY DEF

--- lib/plot.py
+++ lib/plot.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 
+from src.utils import get_project_root
+root = get_project_root()
+
 def make_cloud_plot(df,ch:int,ov:int,save:bool=True,debug:bool=False):
     fig = plt.figure()
     plt.plot(df["DeltaT"],df["Amplitude"],'.',markersize=1)
@@ -11,7 +14,7 @@ def make_cloud_plot(df,ch:int,ov:int,save:bool=True,debug:bool=False):
     plt.ylim(0,0.1)
     plt.ylabel('Amplitude [mV]')
     plt.xlabel('$\Delta T$ [s]')
-    if save: plt.savefig("../images/DC_Amp_vs_DeltaT_"+str(ov)+"_"+str(ch)+".png",dpi=300)
+    if save: plt.savefig(f"{root}/images/DC_Amp_vs_DeltaT_{ov}_{ch}.png",dpi=300)
     return fig
 
 def make_hist_plot(df,ch:int,ov:int,save:bool=True,debug:bool=False):
@@ -21,5 +24,5 @@ def make_hist_plot(df,ch:int,ov:int,save:bool=True,debug:bool=False):
     plt.semilogx()
     plt.ylabel('Amplitude [mV]')
     plt.xlabel('$\Delta T$ [s]')
-    if save: plt.savefig("../images/DC_DeltaT_hist_"+str(ov)+"_"+str(ch)+".png",dpi=300)
+    if save: plt.savefig(f"{root}/images/DC_DeltaT_hist_{ov}_{ch}.png",dpi=300)
     return fig
