2f6f5fa472cec729e6da3098bc206de6d7bdfe88
mantheys <sergio.manthey@ciemat.es>
2024-02-13 18:14:23 +0100

    MAKE PLOTS RUN OVER ALL DF SUBSETS

--- lib/plot.py
+++ lib/plot.py
@@ -3,26 +3,41 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 
+from itertools import product
 from src.utils import get_project_root
 root = get_project_root()
 
-def make_cloud_plot(df,ch:int,ov:int,save:bool=True,debug:bool=False):
-    fig = plt.figure()
-    plt.plot(df["DeltaT"],df["Amplitude"],'.',markersize=1)
-    plt.semilogx()
-    plt.xlim(1e-6,20)
-    plt.ylim(0,0.1)
-    plt.ylabel('Amplitude [mV]')
-    plt.xlabel('$\Delta T$ [s]')
-    if save: plt.savefig(f"{root}/images/DC_Amp_vs_DeltaT_{ov}_{ch}.png",dpi=300)
+def make_cloud_plot(df,save:bool=True,debug:bool=False):
+    numbers = df['Number'].unique()
+    labels = df['Set'].unique()
+    ovs = df['OV'].unique()
+    chs = df['Channel'].unique()
+    for n, label, ov, ch in product(numbers, labels, ovs, chs):
+        data = df[(df['Number'] == n) & (df['Set'] == label) & (df['OV'] == ov) & (df['Channel'] == ch)]
+        fig = plt.figure()
+        plt.plot(data["DeltaT"],data["Amplitude"],'.',markersize=1)
+        plt.semilogx()
+        plt.xlim(1e-6,20)
+        plt.ylim(0,0.1)
+        plt.ylabel('Amplitude [mV]')
+        plt.xlabel('$\Delta T$ [s]')
+        if save: plt.savefig(f"{root}/images/{n}/{label}/{n}_{label}_DC_Amp_vs_DeltaT_{ov}_{ch}.png",dpi=300)
+    
     return fig
 
-def make_hist_plot(df,ch:int,ov:int,save:bool=True,debug:bool=False):
-    fig = plt.figure()
-    bins=np.logspace(-6,1,100)
-    plt.hist(df["DeltaT"],bins=bins,histtype='step',log=True);
-    plt.semilogx()
-    plt.ylabel('Amplitude [mV]')
-    plt.xlabel('$\Delta T$ [s]')
-    if save: plt.savefig(f"{root}/images/DC_DeltaT_hist_{ov}_{ch}.png",dpi=300)
+def make_hist_plot(df,save:bool=True,debug:bool=False):
+    numbers = df['Number'].unique()
+    labels = df['Set'].unique()
+    ovs = df['OV'].unique()
+    chs = df['Channel'].unique()
+    for n, label, ov, ch in product(numbers, labels, ovs, chs):
+        data = df[(df['Number'] == n) & (df['Set'] == label) & (df['OV'] == ov) & (df['Channel'] == ch)]
+        fig = plt.figure()
+        bins=np.logspace(-6,1,100)
+        plt.hist(data["DeltaT"],bins=bins,histtype='step',log=True);
+        plt.semilogx()
+        plt.ylabel('Amplitude [mV]')
+        plt.xlabel('$\Delta T$ [s]')
+        if save: plt.savefig(f"{root}/images/{n}/{label}/{n}_{label}_DC_DeltaT_hist_{ov}_{ch}.png",dpi=300)
+    
     return fig
