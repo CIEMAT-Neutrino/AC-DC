46c1ee6ac5d5d4d720048c84b1aadd6873a66d7d
mantheys <sergio.manthey@ciemat.es>
2024-02-13 12:17:51 +0100

    UPDATE SETUP SCRIPT

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,13 +1,8 @@
-# This script is used to setup the environment for the project.
-
 # Make sure that you run this script from the root directory of the project by running the following command:
 # $ source setup/setup.sh
 
-# Print pwd to confirm that the script is running from the correct directory.
-echo "The current working directory is: $(pwd)"
 # Ask the user to confirm that the script is running from the correct directory.
-echo "Is the current working directory the root directory of the project? (y/n)"
-# Read the user's response.
+echo "Is the current working directory $(pwd) the root directory of the project? (y/n)"
 read response
 # If the user's response is not "y", then exit the script.
 if [ "$response" != "y" ]; then
@@ -16,8 +11,13 @@ if [ "$response" != "y" ]; then
 fi
 
 # Create the necessary directories.
-mkdir -p images
 mkdir -p data
+
+# Try to unmount the data directory
+if [ -d "data" ]; then
+    echo "unmounting data directory..."
+    fusermount -u data
+fi
 
 # Mount the data directory to the correct location.
 directories=("data")
@@ -27,6 +27,8 @@ for dir in "${directories[@]}"; do
         echo "$dir symlink already exists"
     else
         echo "generating symlink for $dir..."
-        sshfs ${USER}@gaeuidc1.ciemat.es:/pc/choozdsk01/palomare/ACDC/FBK_Preproduction "$dir"
+        sshfs ${USER}@gaeuidc1.ciemat.es:/pc/choozdsk01/palomare/ACDC/VUV4 "$dir"
     fi
 done
+mkdir -p data/images
+mkdir -p data/analysis
